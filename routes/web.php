1d709c0b245881ebcaf1d5e14b44b272450713bc
Jay-Khakim <mgmediajay@gmail.com>
2020-10-28 20:39:50 +0500

    localisation done

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/en');
+
+Route::group(['prefix' => '{language}'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
